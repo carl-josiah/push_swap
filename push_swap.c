72f62cd6e9834545e7ddd61cac790c556e91563b
Carl Castro <ccastro@lab2r1s5.42abudhabi.ae>
2025-05-23 19:05:35 +0400

    added libft

--- push_swap.c
+++ push_swap.c
@@ -6,24 +6,32 @@
 /*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 13:58:39 by ccastro           #+#    #+#             */
-/*   Updated: 2025/05/23 14:45:19 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/05/23 16:16:45 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc.h"
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	t_list	*lst;
 
 	lst = NULL;
+	if (ac > 0)
+		printf("ac:%d\n", ac - 1);
 	if (ac <= 2)
+	{
+		//ft_printf(2, "%s\n", ARGER01);
+		//write(2, "Error: not enough arguments\n", ft_strlen);
 		exit(EXIT_FAILURE);
+	}
 	lst = parsing(ac, av);
 	if (!lst)
 	{
 		write(2, "Error\n", 6);
 		exit(EXIT_FAILURE);
 	}
+	ft_free_lst(lst);
 	return (0);
 }
