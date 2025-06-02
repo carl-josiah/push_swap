5a5110de00c568a6f3463fada438e22a49a24b90
Carl Castro <ccastro@lab2r3s7.42abudhabi.ae>
2025-06-02 15:29:29 +0400

    fixed leak

--- sort_stack.c
+++ sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 19:14:21 by ccastro           #+#    #+#             */
-/*   Updated: 2025/05/30 21:45:23 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/06/02 15:28:51 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	sort_stack(t_list *list_of_num)
 
 	init_stack(&a, &b, list_of_num);
 	if (is_sorted_ascending(&a))
-		return (1);
+		return (free_list(&(a.head)), 1);
 	if (a.size == 2)
 		sort_2(&a);
 	else if (a.size == 3)
